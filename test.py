49dd84ca6c1fa1df6ebf6612c73e94a9893b2ba8
Daniel Kulms <danielkulms@Air-von-Daniel.fritz.box>
2023-10-22 23:46:25 +0200

    changed to code that will update only tags column

--- test.py
+++ test.py
@@ -1,251 +1,95 @@
+import calendar
+import datetime as dt
 import requests
+import sqlite3
+import time
+import re
 
 from bs4 import BeautifulSoup
+from helpers import ScrapeArchive, ScrapeArticle
+
 
 def main():
-    #url = input("Enter Tagesschau-URL: ")
-    archiv_url = "https://www.tagesschau.de/archiv?datum="
-    datum = "2023-10-09"
-    #url = "https://www.tagesschau.de/wissen/klima"
-    url = archiv_url+datum
-    print(url)
-    #search_string = input("What word are you looking for? :")
     search_string = "klima"
+    
+    year = 2023
+
+    # Implementing Sqlite3
+    con = sqlite3.connect("tagesschau.db")
+    db = con.cursor()
+    """
+    l = range(1, 10)    # last value exclusive!
+    for i in l:
+        month = i
+
+        num_days = calendar.monthrange(year, month)[1]
+        days = [dt.date(year, month, day) for day in range(1, num_days+1)]
+
+        for day in days:
+            # Create day string
+            date = dt.datetime.strftime(day, '%Y-%m-%d')
+            # Create URL
+            archive_url = "https://www.tagesschau.de/archiv"
+            url_extention = "?datum="
+            url = archive_url+url_extention+date
+    """
+    url = "https://www.tagesschau.de/archiv?datum=2023-09-27"
+    date = "2023-09-27"
+
+    # Request url and get bs4soup
     r = requests.get(url)
     if r.status_code != 200:
         raise ValueError
-    soup = BeautifulSoup(r.text, 'html.parser')
+    soup = BeautifulSoup(r.text, 'html.parser')   
 
-    # Integration von Class
-    archive = ScrapeArchive(soup, datum, search_string)
-    print(archive.n_articles)
-    #for article in archive.html_articles:
-    #    print(article.get('href'))
+    # Running archiveScraper
+    archive = ScrapeArchive(soup, date, search_string)
+    
+    # List for links not used in analysis
+    unused_links = []
 
-    #category = ScrapeCategory(soup, datum, search_string)
-    #print(category.n_articles)
-    #print(category.raw_articles[0].prettify())
-    #for _ in category.teasers:
-    #    print(_['link'])
-        #print(_['label'])
-        #print(_['topline'])
-    #    pass
-
-    #article = ScrapeArticle(category.teasers[0], search_string)
-    #print(category.teasers)
-    #for link in category.teasers[:3]:
-    #    x = ScrapeArticle(link, search_string)
-    #    print(x)
-        #print(link["link"])
-
-    #print(article)
-    #print(len(category.raw_articles))
-    #for article in category.raw_articles:
-    #    print(article.prettify())
-    #print(category.raw_articles)
-    #print(category.links)
-    #for _ in category.links:
-    #    print(_)
-
-    """
-    # Links
-    links = soup.find_all("a", class_="teaser__link")
-    print(len(links))
-    #print(links[0].prettify())
-    authors = []
-    teasers = []
-    for link in links:
-        p = link.find('p')
-        sub_p = p.find_all()
-        for _ in sub_p:
-            #tag = _.name
-            if _.name == "em":
-                authors.append(_.text.strip("Von "))
-            _.decompose()
-            #print(_.name)
-        teasers.append(p.text.strip())
-    counter = []
-    # Teaser
-
-
-    # das funktioniert!
-    for _ in links:
-        p = _.find('p')
-        span = p.span
-        em = p.em
-        try:
-            span.decompose()
-            em.decompose()
-        except:
+    # Using articleScraper
+    for teaser in archive.teaser_list:
+        # Check if article is a Tagesschau original
+        if not teaser["link"].startswith("https://www.tagesschau.de"):
+            unused_links.append(teaser["link"])
             continue
-        #print(p.text.strip())
-    """
+        # Skip if article is already in DB
+        article_url = teaser["link"]
+        a = requests.get(article_url)
+        soup = BeautifulSoup(a.text, 'html.parser')
+        if soup.find('article') == None:
+            continue
+        raw_article = soup.find('article')
+        seitenkopf = raw_article.find('div', class_=('seitenkopf'))
+        topline_label_raw = seitenkopf.find('span', class_=re.compile('label--small'))
+        try:
+            db.execute("UPDATE articles SET topline_label = ? WHERE url = ?;", [topline_label_raw.strong.text.strip()], [teaser['link']])
+        except:
+            try:
+                db.execute("UPDATE articles SET topline_label = ? WHERE url = ?;", [topline_label_raw.text.strip()], [teaser['link']])
+            except:
+                db.execute("UPDATE articles SET topline_label = ? WHERE url = ?;", [None, teaser['link']])
+        # commit changes to db
+        con.commit()
 
-### ich brauche ein dict auf article ebene (weile alle artikel können anderes html layout haben)
-#### in dem dict sind alle keys einerseits
-#### und alle gefundenen values aus dem article andererseits
-#### default ist ""
+                # sleep counter of 1 second
+                #time.sleep(1)
+    print(f"On {date} a total of {len(archive.teaser_list)} articles were analysed")
 
-class ScrapeArchive():
-    def __init__(self, soup, datum, search_string):
-        self.soup = soup
-        self.date = datum
-        self.search_string = search_string
-        self.html_articles = self.get_articles()
-        self.n_articles = len(self.html_articles)
+    # Close DB
+    con.close()
 
-
-    def get_articles(self):
-        links = self.soup.main.find_all('a')
-        for link in links:
-            if "teaser" and "link" in link['class'][0]:
-                print(link['class'][0])
-        return [0,1]
-
-
-
-class ScrapeCategory():
-    def __init__(self, soup, search_string) -> None:
-        self.soup = soup
-        self.search_string = search_string
-        self.raw_articles = self.get_articles()
-        self.n_articles = len(self.raw_articles)
-        self.teasers = []
-        for article in self.raw_articles:
-            d_article = {
-                #"topline": self.get_topline(article),
-                #"headline": self.get_headline(article),
-                #"label": self.get_label(article),
-                #"shorttext": self.get_shorttext(article),
-                "link": self.get_link(article),
-                #"search_string_found": self.analyze_teaser(self.get_shorttext(article)),
-            }
-            self.teasers.append(d_article)
-            #if d_article["label"] == "Video":
-            #    print("Video found!")
-            #    continue
-            #else:
-            #    self.teasers.append(d_article)
-
-        # counter logic is not state of the art...
-        """
-        self.counter = 0
-        for teaser in self.teasers:
-            if teaser["search_string_found"] == True:
-                self.counter += 1
-        """
-
-    def __str__(self):
-        return "nothing"
-        return f"The category has {self.n_articles} teasers in which the search string was found {self.counter} times => {round((self.counter / self.n_articles * 100), 1)}% occurence"
-
-
-
-    def get_articles(self):
-        teaser_links = []
-        for link in self.soup.main.find_all("a"):
-            keyword = "teaser"
-            if keyword in link['class'][0]:
-                teaser_links.append(link['class'][0])
-        d_teaser_links = {}
-        for link in teaser_links:
-            if link not in d_teaser_links.keys():
-                d_teaser_links["class"]  = link
-        return self.soup.main.find_all("a", d_teaser_links)
-
-
-    """
-    def get_articles(self):
-        # teaser-xs is a link without a teaser text
-        classes = {"teaser__link", "teaser-xs__link"}
-        # find all links in main
-        teaser_links = []
-        for _link in self.soup.main.find_all("a"):
-            #print(_link.get('href'))
-            # shows class names for all links in main
-            keyword = "teaser"
-            #print(_link['class'][0])
-            if keyword in _link['class'][0]:
-                teaser_links.append(_link['class'][0])
-        d_teaser_links = {}
-        for link in teaser_links:
-            if link not in d_teaser_links.keys():
-                d_teaser_links["class"]  = link
-        print(d_teaser_links)
-        # shows no of links defined in classes
-        #print(len(self.soup.main.find_all("a", class_=classes)))
-        # working version for links with teaser-text
-        return self.soup.find_all("a", d_teaser_links)
-        #return self.soup.find_all("a", class_="teaser__link")
-        # returns both type of links
-        #return self.soup.find_all("a", class_={"teaser__link", "teaser-xs__link"})
-    """
-
-    def get_topline(self, article):
-        teaser_toplines = []
-        for topline in article.span['class']:
-            keyword = "topline"
-            if keyword in topline['class'][0]:
-                teaser_toplines.append(topline['class'][0])
-        return article.find('span', {'class': 'teaser__topline'}).text
-
-    def get_headline(self, article):
-        return article.find('span', {'class': 'teaser__headline'}).text
-
-    def get_label(self, article):
-        label = article.find('span', {'class': 'label label--small'})
-        if label is not None:
-            return label.text.strip()
-
-    def get_link(self, article):
-        link = article.get('href')
-        if link.startswith('https:') == False:
-            link = "https://www.tagesschau.de" + link
-        return link
-
-    def get_shorttext(self, article):
-        shorttext = article.find('p')
-        sub_p = shorttext.find_all()
-        for _ in sub_p:
-            _.decompose()
-        return shorttext.text.strip()
-
-    def analyze_teaser(self, shorttext):
-        if self.search_string in shorttext.lower():
-            return True
-        else:
-            return False
-
-
-class ScrapeArticle():
-    def __init__(self, teaser, search_string):
-        self.teaser = teaser
-        self.search_string = search_string
-        self.raw_article = self.get_website(teaser["link"])
-        self.teaser["metadata"] = self.raw_article.find("p", {"class": "metatextline"}).get_text().strip("Stand: ")
-        self.paragraphs = []
-        self.get_paragraphs()
-        self.teaser["paragraphs"] = self.paragraphs
-        self.teaser["search_string_paragraph"] = self.analyze_paragraphs()
-        #for k in self.teaser:
-        #    print(f"{k}: {teaser[k]}")
-
-    def __str__(self):
-        return f"Was the search string found in the article '{self.teaser['headline']}'? => {self.teaser['search_string_paragraph']}"
-
-    def get_website(self, url):
-        article = requests.get(url)
-        return BeautifulSoup(article.text, 'html.parser')
-
-    def get_paragraphs(self):
-        for _ in self.raw_article.article.find_all("p", {"class": "textabsatz"}):
-            self.paragraphs.append(_.get_text())
-
-    def analyze_paragraphs(self):
-        for _ in self.teaser["paragraphs"]:
-            if self.search_string in _.lower():
-                return True
-        return False
+    # Print list of links that were not analysed
+    if len(unused_links) > 1:
+        print(f"The following {len(unused_links)} links are not Tagesschau originals and were not analyzed")
+        for link in unused_links:
+            print(link)
+    elif len(unused_links) == 1:
+        print(f"The following link is not a Tagesschau original and was not analyzed")
+        for link in unused_links:
+            print(link)
+    
 
 if __name__ == "__main__":
     main()
